0ef9328fcdad88378b0adba62f02fa9e2f1684fc
Rehnan Carolino <rehnancarolino@live.com>
2015-06-02 19:36:21 -0300

    Routes moved to into admin name space

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
 
-
-  get 'sessions/new'
-
   get 'home/index'
-  get '/admin' => 'admin/home#index'
-
-  get    'login'   => 'admin/sessions#new'
-  post   'login'   => 'admin/sessions#create'
 
   namespace :admin do
+    root to: 'home#index'
+    get    'login'   => 'sessions#new'
+    post   'login'   => 'sessions#create'
+
     resources :contests
   end
 
