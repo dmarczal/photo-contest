5dff1f1a6738a17b36b95c71f0c469144422e9c8
Gustavo Pchek <ghpk88@gmail.com>
2015-06-14 20:39:31 -0300

    Contest listing in home and fixing photographer page css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+  	@currentContest = Contest.last
+  	@oldContests = Contest.last(3)
+	# @contests = Contest.where(":opening_enrollment < ?", Time.zone.now)
   end
 end
