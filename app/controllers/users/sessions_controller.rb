2f81d426f9f0f69933cab73623de24e6c7b0f5f9
Rehnan Carolino <rehnancarolino@live.com>
2015-06-15 17:28:55 -0300

    Bugfixed: checkbox in login form | Implemented: add link to register and refactoring

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,15 @@
 class Users::SessionsController < Devise::SessionsController
+  #before_action :logged_in_user
+
+  #private
+   # Confirms a logged-in user.
+    #def logged_in_user
+      #unless user_signed_in?
+        #flash[:danger] = "Por favor, insira seu login!"
+        #redirect_to new_user_session_path
+      #end
+    #end
+    
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
