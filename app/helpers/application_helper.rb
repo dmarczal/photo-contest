0085b8860a8d7cba9ee641041a651bc2a7464c9e
Rehnan Carolino <rehnancarolino@live.com>
2015-06-29 17:47:49 -0300

    Add link in image to show current contest

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -73,7 +73,7 @@ end
 
   # show link to register in any contest if it is enrollment open
   def contest_link_register(contest)
-    (contest.open_enrollment?) ? link_to('Inscrever-me', new_participant_path(:contest_id => contest.id), :method => :get) : ''
+    (contest.open_enrollment?) ? link_to('Inscrever-me', new_participant_path(:contest_id => contest.id), :method => :get) : link_to('Visitar a página do concurso ', contest_path(contest)) 
   end
 
   # show approved label status 
