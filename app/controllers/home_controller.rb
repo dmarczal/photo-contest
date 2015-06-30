e146ac6580712b7e9d3a1ad7ac7eccd04d56b245
André Felipe Silveira <andrefelipesilveira@yahoo.com.br>
2015-06-30 18:38:52 -0300

    participant approved show in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   	@current_contest = Contest.current
     @old_contests = Contest.closed_home
-    @participants = Participant.all.order(created_at: :desc).limit(10)
-  	#@oldContests = Contest.all
+    @participants = Participant.where(status: 2).order(created_at: :desc).limit(10)
+
   end
 end
