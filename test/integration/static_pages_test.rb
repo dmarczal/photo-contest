ee71fa42780c08e395b5eda7d0b9091aa8e52997
helbert-rocha <helberti@outlook.com>
2015-11-12 16:13:43 -0200

    Finish the tasks

--- test/integration/static_pages_test.rb
+++ test/integration/static_pages_test.rb
@@ -2,15 +2,7 @@ require 'test_helper'
 
 class StaticPagesTest < ActionDispatch::IntegrationTest
   def setup
-  	@page  = pages(:about)
     @pages = Page.order(:name)
-  end
-
-  test "should get page about" do
-    get '/about'
-    assert_response :success
-    assert assigns(:page)
-    assert_template 'static_pages/index'
   end
 
   test "should return excpection when page not found" do
