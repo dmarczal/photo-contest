4c5de2d7277794619e7d810932779c36ef70bff5
Erico Dias <ericodias1@gmail.com>
2015-11-11 22:04:39 -0200

    general adjusts

--- test/controllers/admin/home_controller_test.rb
+++ test/controllers/admin/home_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class Admin::HomeControllerTest < ActionController::TestCase
-  #test "should get index" do
-  #  get :index
-  #  assert_response :success
-  #end
+  test "should get index" do
+   get :index
+   assert_redirect_to new_user_session
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
+require 'simplecov'
+SimpleCov.start
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
