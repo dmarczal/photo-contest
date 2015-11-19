a2dea122f318a6d237e9bb0f614aba8650f276a8
helbert-rocha <helberti@outlook.com>
2015-11-18 22:02:49 -0200

    Testing user login, user name on dropdown after login, and user logout

--- test/integration/users_name_test.rb
+++ test/integration/users_name_test.rb
@@ -23,12 +23,25 @@ class UsersNameTest < ActionDispatch::IntegrationTest
   end
 
   test "login with invalid information" do
-    get '/user/sign_in'
-    assert_template '/users/sign_in'
+    get '/users/sign_in'
     post '/users/sign_in', {user: { login: @user.username,
                                     password: "12345678910"} }
     assert_not flash.empty?
     get root_path
+  end
+
+  test "login with valid information followed by logout" do
+    get '/users/sign_in'
+    assert_response :success
+    post '/users/sign_in', {user: { login: @user.username,
+                                    password: "12345678"} }
+
+    follow_redirect!
+    assert_template root_path
+    assert_select "ul>li>a.dropdown-toggle.user-name", @user.name
+    delete '/users/sign_out'
+    follow_redirect!
+   assert_template root_path
   end
 
   test "user name on dropdown" do
