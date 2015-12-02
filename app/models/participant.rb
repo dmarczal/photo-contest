76c346fd13ead86edfbe61d6c63c503d59633453
Felipe Gomes <felipegomes.dev@gmail.com>
2015-12-02 18:43:49 -0200

    Tarefa 2 - Validar envio da foto

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,34 +1,33 @@
+include Paperclip::Glue
+
 class Participant < ActiveRecord::Base
-  include Paperclip::Glue
   belongs_to :user
   belongs_to :contest
   has_many :votes
 
   has_attached_file(:picture, {
-                      :styles => { :medium => "600x800>", :thumb => "100x100>",
-                                 :large => '1200x800>' },
+                      :styles => { :medium => "600x800>", :thumb => "100x100>", :large => '1200x800>' },
                       :default_url => "placeholder.png"
                     }.merge(PaperclipStorageOption.options))
 
   enum status: [ :pending, :failed, :approved ]
   
   #Validation to presence true and unique ids
-  validates_uniqueness_of :user_id, :message => 'Você já está inscrito neste concurso!', :scope => :contest_id
   validates :title, :description, :accepted_term, presence: true
-
+  validates_uniqueness_of :user_id, :message => 'Você já está inscrito neste concurso!', :scope => :contest_id
   
   #Validation to minimum - maximum caracters lenght 
-  validates :title, length: { minimum: 3 }
-  validates :title, length: { maximum: 50 }
-  validates :description, length: { minimum: 10 }
-  validates :description, length: { maximum: 500 }
+  validates :title, length: { minimum: 3, maximum: 50}
+  validates :description, length: { minimum: 10, maximum: 500}
 
   #Validation to picture properties
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
-  #validates :picture, :attachment_presence => true
-  validates_attachment :picture, 
-                        :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] },
-                        :size => { :in => 0..20.megabytes }
+  validates_attachment :picture,
+                       :content_type => {
+                          :content_type => ["image/jpeg", "image/gif", "image/png"]
+                       },
+                       :size => { :in => 0..20.megabytes }
+  validates :picture, :attachment_presence => true
 
   #Check if inscription is between deadline
   def between_deadline?
@@ -52,9 +51,7 @@ class Participant < ActiveRecord::Base
       end
       
     end
-    #votes.sort 
-    #binding.pry
 
   end
-
+  
 end
