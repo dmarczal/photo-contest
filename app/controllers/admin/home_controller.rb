551c4db264ceecb05463d816e768055d3e3b7165
mario <jrmario99@gmail.com>
2015-12-04 20:10:06 -0200

    first part

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -6,6 +6,15 @@ class Admin::HomeController < Admin::ApplicationController
   before_action :admin_user
 
   def index
+    @contests =  Contest.opening_enrollment
+    @not_approved_participants = Participant.pending.where(contest_id: @contests.ids)
+
+    @contests_opening = Contest.opening
+    @participants = Participant.approved.where(contest_id: @contests_opening.ids)
+  #  if @contests_opening.open?
+  #    @partial_podium = @participants.joins("LEFT OUTER JOIN votes ON votes.participant_id = participants.id").group("participants.id").order("count(votes.participant_id) desc")
+  #  end
+
   end
 
 end
