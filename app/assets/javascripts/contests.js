5cb6bc600af3d156c5cbadaf7a7617624614875e
mario <jrmario99@gmail.com>
2015-12-05 17:01:09 -0200

    ShareFacebook

--- app/assets/javascripts/contests.js
+++ app/assets/javascripts/contests.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
+     js.src = "//connect.facebook.net/pt_BR/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 });
